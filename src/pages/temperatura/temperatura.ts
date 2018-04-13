0523b67aca5fcc5b3a26415950519622948b2b05
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.edu.br>
2018-04-13 19:16:15 -0300

    Colocando cor diferene na temperatura e  aumentando tamanho

--- src/pages/temperatura/temperatura.ts
+++ src/pages/temperatura/temperatura.ts
@@ -65,6 +65,21 @@ export class TemperaturaPage {
         datasets: [{
           label: 'Temperatura',
           data: [this.temperatura.temperatura],
+          fill: false,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 5,
+          pointHitRadius: 15,
 
         }],
 
