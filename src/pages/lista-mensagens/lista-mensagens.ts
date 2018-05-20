ed2db9965976e4297e823f6aa61558c9fc7ff4b7
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-05-20 15:43:27 -0300

    Layout da pagina de lista mensagens quase finalizado

--- src/pages/lista-mensagens/lista-mensagens.ts
+++ src/pages/lista-mensagens/lista-mensagens.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { MensagemService } from '../../services/domain/mensagem.service';
+import { UsuarioService } from '../../services/domain/usuario.service';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the ListaMensagensPage page.
@@ -19,7 +21,8 @@ export class ListaMensagensPage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public mensagemService:MensagemService,
-              public alertCtrl:AlertController) {
+              public alertCtrl:AlertController,
+              public usuarioService:UsuarioService) {
   }
 
   ionViewDidLoad() {
@@ -29,6 +32,9 @@ export class ListaMensagensPage {
   obterMensagens(){
     this.mensagemService.findAll().subscribe((mensagens)=>{
       this.mensagens = mensagens;
+      this.mensagens.forEach(mensagem => {
+        mensagem = this.getImageIfExists(mensagem);
+      });
     })
   }
 
@@ -58,6 +64,26 @@ export class ListaMensagensPage {
        this.obterMensagens()
      })
    }
+  //  buscarFuncionarios(){
+  //   this.usuarioService.findAll().subscribe((response => {
+
+  //     this.funcionarios = response;
+  //     this.funcionarios.forEach(funcionario => {
+  //       funcionario = this.getImageIfExists(funcionario);
+  //     });
+  //   }), error => {
+
+  //   })
+  // }
+  getImageIfExists(mensagem) {
+    this.usuarioService.getImageFromBucket(mensagem['clienteId'])
+      .subscribe(response => {
+        mensagem.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${mensagem['clienteId']}.jpg`;
+        return mensagem;
+      },
+        error => { });
+  }
+
   }
 
 
