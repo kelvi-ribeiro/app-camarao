ab92849e0dd35fcbe4f77ee0dda65a68e0956a6b
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-05-25 21:58:30 -0300

    Ajustando url padrao

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
   // baseUrl: "https://app-macuxi-backend-teste.herokuapp.com//",
-  baseUrl: " https://0b49ce85.ngrok.io",
+  baseUrl: "http://localhost:8081",
   bucketBaseUrl:"https://s3-sa-east-1.amazonaws.com/app-camarao"
 }
 
