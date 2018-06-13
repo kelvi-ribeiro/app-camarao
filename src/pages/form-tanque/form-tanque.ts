8e95afddda34e6f1276eeaafb161ed9c525eca7e
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-06-13 02:36:42 -0300

    criando script de build automatico de browser e testando api de GeoLocation

--- src/pages/form-tanque/form-tanque.ts
+++ src/pages/form-tanque/form-tanque.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Globals } from '../../globals.array';
+import { Geolocation } from '@ionic-native/geolocation';
 
 /**
  * Generated class for the FormTanquePage page.
@@ -21,7 +22,15 @@ export class FormTanquePage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public formBuilder: FormBuilder,
-              public globals:Globals) {
+              public globals:Globals,
+              public geolocation:Geolocation)
+               {
+                this.geolocation.getCurrentPosition().then((resp) => {
+                  console.log(resp.coords.latitude)
+                   console.log(resp.coords.longitude)
+                 }).catch((error) => {
+                   console.log('Error getting location', error);
+                 });
 
           this.usuarioId = this.globals.usuario.id
           this.formGroup = this.formBuilder.group({
