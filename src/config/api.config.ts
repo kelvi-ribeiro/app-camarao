6259308217874ded6923b07331ae77e73c252e87
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-06-14 04:34:20 -0300

    Adicionando urls do Geolocation

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,9 @@
 export const API_CONFIG = {
-   baseUrl: "https://330f6520.ngrok.io",
+   baseUrl: "http://98b5651f.ngrok.io",
  // baseUrl: "http://localhost:8081",
-  bucketBaseUrl:"https://s3-sa-east-1.amazonaws.com/app-camarao"
+  bucketBaseUrl:"https://s3-sa-east-1.amazonaws.com/app-camarao",
+  urlGeolocation:"https://maps.googleapis.com/maps/api/geocode/json?",
+  apiKeyGeolocation:"AIzaSyAgkZp1A4zstakjAm6rMoWzpvI-2l3anMM"
 }
+
 
