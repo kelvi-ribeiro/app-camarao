c1da6359ae94494b088ae19369337fb875c4abd0
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-12-22 03:50:37 -0200

    tirando ip default do ngrok

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-   baseUrl: "http://cdf5c6c1.ngrok.io",
- // baseUrl: "http://localhost:8081",
+ //  baseUrl: "http://cdf5c6c1.ngrok.io",
+  baseUrl: "http://localhost:8081",
   bucketBaseUrl:"https://s3-sa-east-1.amazonaws.com/app-camarao",
   urlGeolocation:"https://maps.googleapis.com/maps/api/geocode/json?",
   apiKeyGeolocation:"AIzaSyAgkZp1A4zstakjAm6rMoWzpvI-2l3anMM"
